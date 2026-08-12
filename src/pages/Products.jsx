1434c9b92e8d5c7762f7933145c0f6bc892c42f4
jenish-ml <jenishpunjiri@gmail.com>
2026-08-12 13:02:26 +0530

    Delete Method

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -58,6 +58,16 @@ const Products = () => {
     //         .catch(err => console.log(err))
     // }
     
+    const deleteProduct = (id) => {
+        if (!window.confirm('Are you sure you want to delete this product?')) return
+
+        axios.delete(`${api}/${id}`)
+            .then(res => {
+                setProducts(products.filter(product => product.id !== id))
+            })
+            .catch(err => console.log(err))
+    }
+
   return (
     <div className="product-page">
       <h1>Product List</h1>
@@ -69,6 +79,7 @@ const Products = () => {
               <img src={prod.image} alt={prod.title} className="product-image" />
             </div> 
              <p className="product-price">${prod.price.toFixed(2)}</p> */}
+            <button onClick={() => deleteProduct(prod.id)}>Delete</button>
           </div>
         ))}
       </div><br />
