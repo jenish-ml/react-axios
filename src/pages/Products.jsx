8773b8914721ecd64ef1d5dbb2faf9041835751a
jenish-ml <jenishpunjiri@gmail.com>
2026-08-12 12:24:21 +0530

    PUT MEthod

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,15 +3,16 @@ import { useState, useEffect } from 'react'
 import './Product.css'
 
 const Products = () => {
-    const [product, setProduct] = useState([])
+    const [products, setProducts] = useState([])
     const [title, setTitle] = useState('')
+    const [editId, setEditId] = useState(null)
 
     const api = 'https://fakestoreapi.com/products'
 
     useEffect(() => {
         axios.get(api)
         .then(res => {
-            setProduct(res.data)
+            setProducts(res.data)
         })
         .catch(err => {
             console.log(err)
@@ -20,39 +21,60 @@ const Products = () => {
 
     
     const addProduct = () => {
-        axios.post(api, {
-            title: title
-            
-        })
+        axios.post(api, { title })
         .then(res => {
-            setProduct([...product, res.data]),
+            setProducts([...products, res.data])
             setTitle('')
         })
         .catch(err => {     
             console.log(err)
         })
     }
-    console.log(product);
+
+    const startEdit = (product) => {
+        setEditId(product.id)
+        setTitle(product.title)
+    }
+
+    const updateProduct = () => {
+        if (!editId) return
+
+        axios.put(`${api}/${editId}`, { title })
+            .then(res => {
+                setProducts(products.map(product => product.id === editId ? res.data : product))
+                setTitle('')
+                setEditId(null)
+            })
+            .catch(err => console.log(err))
+    }
+
+
     
   return (
     <div className="product-page">
       <h1>Product List</h1>
-
-      <input type="text" placeholder='Enter Product title' value={title} onChange={e => setTitle(e.target.value)} />
-
-      <button onClick={addProduct}>Add product</button>
-
       <div className="product-grid">
-        {product.map(prod => (
-          <div key={prod.id} className="product-card">
+        {products.map(prod => (
+          <div key={prod.id} className="product-card" onClick={() => startEdit(prod)}>
             <h2>{prod.title}</h2>
             {/* <div className="product-image-wrapper">
               <img src={prod.image} alt={prod.title} className="product-image" />
-            </div> */}
-            {/* <p className="product-price">${prod.price.toFixed(2)}</p> */}
+            </div> 
+             <p className="product-price">${prod.price.toFixed(2)}</p> */}
           </div>
         ))}
-      </div>
+      </div><br />
+
+
+     <div>
+       <input type="text" placeholder='Enter Product title' value={title} onChange={e => setTitle(e.target.value)} />
+
+        {editId ? (
+          <button onClick={updateProduct}>Update product</button>
+        ) : (
+          <button onClick={addProduct}>Add product</button>
+        )}
+     </div>
     </div>
   )
 }
