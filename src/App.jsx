2f5d082e1a5527b178ed7527739c190205ce2076
jenish-ml <jenishpunjiri@gmail.com>
2026-08-13 13:03:53 +0530

    Form Validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Products from './pages/Products'
+import RegisterForm from './pages/RegisterForm'
 
 export default function App() {
   return (
     <>
-    <Products />
+    {/* <Products /> */}
+    <RegisterForm />
     </>
   )
 }
---
+++ src/pages/RegisterForm.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { useState } from 'react'
+
+const RegisterForm = () => {
+
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        mobile: '',
+        dob: '',
+        password: '',
+        cpassword: ''
+    })
+
+    const [errors, setErrors] = useState({})
+
+    const validateForm = () => {
+        const newErrors = {}
+
+        if (form.name === '') {
+            newErrors.name = 'Name is required'
+        }
+
+        if (form.email === '') {
+            newErrors.email = 'Email is required'
+        }
+
+        if (form.mobile === '') {
+            newErrors.mobile = 'Mobile is required'
+        }
+
+        if (form.dob === '') {
+            newErrors.dob = 'DOB is required'
+        }
+
+        if (form.password === '') {
+            newErrors.password = 'Password is required'
+        }
+
+        if (form.cpassword === '') {
+            newErrors.cpassword = 'Confirm Password is required'
+        }
+
+        setErrors(newErrors)
+        setForm({name: '', email: '', mobile: '', dob: '', password: '', cpassword: ''})
+    }
+
+    console.log(form);
+    console.log(errors);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        validateForm()
+    }
+    
+  return (
+    <>
+    <h1>Register Form</h1>
+
+    <input type="text" name='name' placeholder='Enter Your Name' value={form.name}
+     onChange = {(e) => setForm({...form, name: e.target.value})}
+    /><br />
+    {errors.name && <span>{errors.name}</span>} <br />
+    <input type="text" name='email' placeholder='Enter Your Email' value={form.email}
+     onChange={(e) => setForm({...form, email : e.target.value})}
+    /><br />
+    {errors.email && <span>{errors.email}</span>} <br />
+    <input type="text" name='mobile' placeholder='Enter Your Mobile' value={form.mobile}
+     onChange={(e) => setForm({...form, mobile : e.target.value})}
+    /><br />
+    <input type="date" name='dob' placeholder='Enter Your DOB' value={form.dob}
+     onChange={(e) => setForm({...form, dob : e.target.value})}
+    /><br />
+    <input type="password" name='password' placeholder='Enter Your Password' value={form.password}
+     onChange={(e) => setForm({...form, password : e.target.value})}
+    /><br />
+    <input type="password" name='cpassword' placeholder='Enter Your Confirm Password' value={form.cpassword}
+     onChange={(e) => setForm({...form, cpassword : e.target.value})}
+    /><br />
+    <button onClick={handleSubmit}>Register</button>
+
+    </>
+  )
+}
+
+export default RegisterForm
