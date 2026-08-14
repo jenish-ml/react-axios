1628ec292a65786479264e27ecc16847bc212bcc
jenish-ml <jenishpunjiri@gmail.com>
2026-08-14 11:24:09 +0530

    Modified

--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const RegisterForm = () => {
 
@@ -41,59 +40,176 @@ const RegisterForm = () => {
             newErrors.cpassword = 'Confirm Password is required'
         }
 
-        setErrors(newErrors)
-    }
+        if (
+            form.password !== '' &&
+            form.cpassword !== '' &&
+            form.password !== form.cpassword
+        ) {
+            newErrors.cpassword = 'Passwords do not match'
+        }
 
-    console.log(form);
-    console.log(errors);
+        setErrors(newErrors)
+        return Object.keys(newErrors).length === 0
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
+
         const isValid = validateForm()
+        console.log(isValid);
+        
 
         if (isValid) {
             console.log('Form submitted successfully')
+
+            setForm({
+                name: '',
+                email: '',
+                mobile: '',
+                dob: '',
+                password: '',
+                cpassword: ''
+            })
         }
-
-        setForm({
-            name: '',
-            email: '',
-            mobile: '',
-            dob: '',
-            password: '',
-            cpassword: ''
-        })
     }
-    
-  return (
-    <>
-    <h1>Register Form</h1>
 
-    <input type="text" name='name' placeholder='Enter Your Name' value={form.name}
-     onChange = {(e) => setForm({...form, name: e.target.value})}
-    /><br />
-    {errors.name && <span>{errors.name}</span>} <br />
-    <input type="text" name='email' placeholder='Enter Your Email' value={form.email}
-     onChange={(e) => setForm({...form, email : e.target.value})}
-    /><br />
-    {errors.email && <span>{errors.email}</span>} <br />
-    <input type="text" name='mobile' placeholder='Enter Your Mobile' value={form.mobile}
-     onChange={(e) => setForm({...form, mobile : e.target.value})}
-    /><br />
-    {errors.mobile && <span>{errors.mobile}</span>} <br />
-    <input type="date" name='dob' placeholder='Enter Your DOB' value={form.dob}
-     onChange={(e) => setForm({...form, dob : e.target.value})}
-    /><br />
-    <input type="password" name='password' placeholder='Enter Your Password' value={form.password}
-     onChange={(e) => setForm({...form, password : e.target.value})}
-    /><br />
-    <input type="password" name='cpassword' placeholder='Enter Your Confirm Password' value={form.cpassword}
-     onChange={(e) => setForm({...form, cpassword : e.target.value})}
-    /><br />
-    <button onClick={handleSubmit}>Register</button>
+    return (
+        <div
+            style={{
+                width: '400px',
+                margin: '50px auto',
+                padding: '20px',
+                border: '1px solid black',
+                borderRadius: '8px'
+            }}
+        >
 
-    </>
-  )
+            <h1>Register Form</h1>
+
+            <input
+                type="text"
+                name="name"
+                placeholder="Enter Your Name"
+                value={form.name}
+                onChange={(e) =>
+                    setForm({ ...form, name: e.target.value })
+                }
+            />
+            <br />
+
+            {errors.name && (
+                <span style={{ color: 'red' }}>
+                    {errors.name}
+                </span>
+            )}
+
+            <br /><br />
+
+
+            <input
+                type="text"
+                name="email"
+                placeholder="Enter Your Email"
+                value={form.email}
+                onChange={(e) =>
+                    setForm({ ...form, email: e.target.value })
+                }
+            />
+            <br />
+
+            {errors.email && (
+                <span style={{ color: 'red' }}>
+                    {errors.email}
+                </span>
+            )}
+
+            <br /><br />
+
+
+            <input
+                type="text"
+                name="mobile"
+                placeholder="Enter Your Mobile"
+                value={form.mobile}
+                onChange={(e) =>
+                    setForm({ ...form, mobile: e.target.value })
+                }
+            />
+            <br />
+
+            {errors.mobile && (
+                <span style={{ color: 'red' }}>
+                    {errors.mobile}
+                </span>
+            )}
+
+            <br /><br />
+
+
+            <input
+                type="date"
+                name="dob"
+                value={form.dob}
+                onChange={(e) =>
+                    setForm({ ...form, dob: e.target.value })
+                }
+            />
+            <br />
+
+            {errors.dob && (
+                <span style={{ color: 'red' }}>
+                    {errors.dob}
+                </span>
+            )}
+
+            <br /><br />
+
+
+            <input
+                type="password"
+                name="password"
+                placeholder="Enter Your Password"
+                value={form.password}
+                onChange={(e) =>
+                    setForm({ ...form, password: e.target.value })
+                }
+            />
+            <br />
+
+            {errors.password && (
+                <span style={{ color: 'red' }}>
+                    {errors.password}
+                </span>
+            )}
+
+            <br /><br />
+
+
+            <input
+                type="password"
+                name="cpassword"
+                placeholder="Enter Your Confirm Password"
+                value={form.cpassword}
+                onChange={(e) =>
+                    setForm({ ...form, cpassword: e.target.value })
+                }
+            />
+            <br />
+
+            {errors.cpassword && (
+                <span style={{ color: 'red' }}>
+                    {errors.cpassword}
+                </span>
+            )}
+
+            <br /><br />
+
+            <button onClick={handleSubmit}>
+                Register
+            </button>
+
+        </div>
+    )
 }
 
 export default RegisterForm
